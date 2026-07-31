a34e566385def039e7545930ef664572687f4724
Wolfgang Fahl <wf@bitplan.com>
2026-07-31 22:10:09 +0200

    adds the three LLL seed datasets per #24: author_identity_wikidata, place_identity_factgrid and line_relation_osm close the wd:P496, fgt:P1073 and osmrel:1852360 leg coverage gaps found on fixit

--- examples/locations/load.sh
+++ examples/locations/load.sh
@@ -2,7 +2,7 @@
 # load the historical locations use case datasets into all functional pyomnigraph backends
 # see https://github.com/WolfgangFahl/pyomnigraphPaper/issues/10
 script_dir=$(dirname "$0")
-for dataset in place_identity_gov place_names_gov place_hierarchy_gov persons_of_place_wikidata
+for dataset in place_identity_gov place_identity_factgrid place_names_gov place_hierarchy_gov persons_of_place_wikidata
 do
   rdfdump -dc "$script_dir/datasets.yaml" -ds $dataset --dump -4o
   omnigraph -dc "$script_dir/datasets.yaml" -ds $dataset -s all --cmd upload count
--- examples/railway/load.sh
+++ examples/railway/load.sh
@@ -2,7 +2,7 @@
 # load the railway use case datasets into all functional pyomnigraph backends
 # see https://github.com/WolfgangFahl/pyomnigraphPaper/issues/10
 script_dir=$(dirname "$0")
-for dataset in relation_stops_osm line_identity_wikidata stations_of_line_rinf station_locality_osm
+for dataset in relation_stops_osm line_identity_wikidata line_relation_osm stations_of_line_rinf station_locality_osm
 do
   rdfdump -dc "$script_dir/datasets.yaml" -ds $dataset --dump -4o
   omnigraph -dc "$script_dir/datasets.yaml" -ds $dataset -s all --cmd upload count
