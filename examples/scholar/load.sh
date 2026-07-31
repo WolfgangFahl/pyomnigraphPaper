da5ec33525a3968dec806d4a2841806623c94725
Wolfgang Fahl <wf@bitplan.com>
2026-07-31 14:04:59 +0200

    adds event_author_papers_wikidata dataset declaration extending scholar load and verify per #7 #10

--- examples/scholar/load.sh
+++ examples/scholar/load.sh
@@ -2,7 +2,7 @@
 # load the scholarly use case datasets into all functional pyomnigraph backends
 # see https://github.com/WolfgangFahl/pyomnigraphPaper/issues/10
 script_dir=$(dirname "$0")
-for dataset in author_papers_dblp coauthor_papers_dblp
+for dataset in author_papers_dblp coauthor_papers_dblp event_author_papers_wikidata
 do
   rdfdump -dc "$script_dir/datasets.yaml" -ds $dataset --dump -4o
   omnigraph -dc "$script_dir/datasets.yaml" -ds $dataset -s all --cmd upload count
