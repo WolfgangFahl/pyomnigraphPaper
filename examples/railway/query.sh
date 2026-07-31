61812bfd0df8712202f9132a9ac232fbc4e718b8
Wolfgang Fahl <wf@bitplan.com>
2026-07-31 11:19:44 +0200

    railway query.sh writes RelationStops.md github markdown, imported to #3

--- examples/railway/query.sh
+++ examples/railway/query.sh
@@ -2,5 +2,8 @@
 # reproduce the railway journey planning use case query results
 # see https://github.com/WolfgangFahl/pyomnigraphPaper/issues/2
 script_dir=$(dirname "$0")
-sparqlquery -ep "$script_dir/endpoints.yaml" -qp "$script_dir/queries.yaml" \
-  -qn RelationStops -en railway-osm -f github
+for query_name in RelationStops
+do
+  sparqlquery -ep "$script_dir/endpoints.yaml" -qp "$script_dir/queries.yaml" \
+    -qn $query_name -en railway-osm -f github > $query_name.md
+done
