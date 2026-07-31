186b0b63f2336bdf786fcce6a8a201a20d99901f
Wolfgang Fahl <wf@bitplan.com>
2026-07-31 13:50:39 +0200

    adds author_papers_dblp dataset declaration with scholar load and verify scripts per #4 #10

---
+++ examples/scholar/load.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# load the scholarly use case datasets into all functional pyomnigraph backends
+# see https://github.com/WolfgangFahl/pyomnigraphPaper/issues/10
+script_dir=$(dirname "$0")
+for dataset in author_papers_dblp
+do
+  rdfdump -dc "$script_dir/datasets.yaml" -ds $dataset --dump -4o
+  omnigraph -dc "$script_dir/datasets.yaml" -ds $dataset -s all --cmd upload count
+done
